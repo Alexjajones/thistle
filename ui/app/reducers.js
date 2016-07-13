da14a6554123c5e0317848858601ec42cd048883
Alex Jones <alexjajones1@gmail.com>
2016-07-14 00:27:00 +0100

    Further work on reducers for cart/bag

--- ui/app/reducers.js
+++ ui/app/reducers.js
@@ -1,24 +1,39 @@
-import { combineReducers } from 'redux';
+//import { combineReducers, createStore } from 'redux';
+const { combineReducers, createStore } = Redux;
 
-const defaultBag = {showing: false, items: []};
-
-const bag = (state = defaultBag, action) => {
+const bag = (state = {showing: false, items: []}, action) => {
     switch (action.type) {
         case 'ADD_ITEM':
-            return Object.assign({}, state, {items: [...state.item, action.item]});
+            return Object.assign({}, {showing: state.showing, items: [...state.items, action.item]});
         case 'REMOVE_ITEM':
             return;
         case 'UPDATE_ITEM':
             return;
         case 'TOGGLE_BAG':
-            return Object.assign({}, state, {showing: !state.showing});
+            return Object.assign({}, {showing: !state.showing, items: state.items});
         default:
             return state;
     }
 };
 
 const App = combineReducers({
-    bag
+    bag: bag
 });
 
-export default App;
+var store = createStore(App);
+
+store.bag.subscribe();
+
+store.dispatch({
+    type: 'ADD_ITEM',
+    item: {name: 'test123'}
+});
+
+
+store.dispatch({
+    type: 'TOGGLE_BAG'
+});
+
+console.log(store.getState());
+
+//export default App;
